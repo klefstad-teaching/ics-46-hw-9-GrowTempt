63adf514395fbf18e79cf0deac6215bc5cfa8028
GrowTempt <vincenthoang111@gmail.com>
2025-03-17 03:52:31 +0000

    adjacency insertdelete

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,15 +3,24 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(Step1, AdjacentSameLength) {
+TEST(Adjacent, SameLength) {
     EXPECT_TRUE(is_adjacent("cat","cot"));
     EXPECT_TRUE(is_adjacent("code","cade"));
     EXPECT_FALSE(is_adjacent("cat","dog"));
     EXPECT_FALSE(is_adjacent("cat","cat"));
 }
 
-TEST(Step1, AdjacentDiffLength) {
-    EXPECT_FALSE(is_adjacent("cat","at")); 
-    EXPECT_FALSE(is_adjacent("cat","chat"));
-    EXPECT_FALSE(is_adjacent("cat","cats"));
+TEST(Adjacent, DiffLength) {
+    EXPECT_TRUE(is_adjacent("cat","at")); 
+    EXPECT_TRUE(is_adjacent("cat","chat"));
+    EXPECT_TRUE(is_adjacent("cat","cats"));
+}
+
+TEST(Adjacent, InsertDelete) {
+    EXPECT_TRUE(is_adjacent("cat","at"));
+    EXPECT_TRUE(is_adjacent("chat","hat"));
+    EXPECT_TRUE(is_adjacent("cat","scat"));
+    EXPECT_TRUE(is_adjacent("data","date"));
+    EXPECT_FALSE(is_adjacent("cat","catsy"));
+    EXPECT_FALSE(is_adjacent("cat","act"));
 }
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -9,26 +9,41 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const string& str1, const string& str2, int d) {
-    if (str1.size() != str2.size()) {
+    if (max(str1.size(), str2.size()) - min(str1.size(), str2.size()) > 1) {
         return false;
     }
-    int diffCount = 0;
-    for (size_t i = 0; i < str1.size(); i++) {
-        if (str1[i] != str2[i]) {
-            diffCount++;
-            if (diffCount > d) return false;
+
+    if (str1.size() == str2.size()) {
+        int diffCount = 0;
+        for (size_t i = 0; i < str1.size(); i++) {
+            if (str1[i] != str2[i]) {
+                diffCount++;
+                if (diffCount > d) return false;
+            }
         }
+        return (diffCount == 1); 
     }
 
-    return (diffCount == 1);
+    const string& sShort = (str1.size() < str2.size()) ? str1 : str2;
+    const string& sLong  = (str1.size() < str2.size()) ? str2 : str1;
+
+    size_t i = 0, j = 0;
+    bool foundDiff = false;
+    while (i < sShort.size() && j < sLong.size()) {
+        if (sShort[i] != sLong[j]) {
+            if (foundDiff) return false;
+            foundDiff = true;
+            j++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    return true;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-    if (word1.size() == word2.size()) {
-        return edit_distance_within(word1, word2, 1);
-    }
-
-    return false;
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string&, const string&, const set<string>&) {
