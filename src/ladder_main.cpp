8c428159b295291847dac839f077d88733544617
GrowTempt <vincenthoang111@gmail.com>
2025-03-17 06:05:51 +0000

    main

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    verify_word_ladder();
+    //verify_word_ladder();
 
-    // if (argc < 3) {
-    //     cerr << "Usage: " << argv[0] << " <start_word> <end_word>\n";
-    //     return 1;
-    // }
-    // string start = argv[1];
-    // string end   = argv[2];
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <start_word> <end_word>\n";
+        return 1;
+    }
+    string start = argv[1];
+    string end   = argv[2];
 
-    // for (auto &c : start) c = tolower((unsigned char)c);
-    // for (auto &c : end)   c = tolower((unsigned char)c);
+    for (auto &c : start) c = tolower((unsigned char)c);
+    for (auto &c : end)   c = tolower((unsigned char)c);
 
-    // set<string> dictionary;
-    // load_words(dictionary, "words.txt");
+    set<string> dictionary;
+    load_words(dictionary, "../src/words.txt");
 
-    // auto ladder = generate_word_ladder(start, end, dictionary);
-    // print_word_ladder(ladder);
+    auto ladder = generate_word_ladder(start, end, dictionary);
+    print_word_ladder(ladder);
 
     return 0;
 }
