4f98bb8c7e516878a14ee884a226472904918b0a
GrowTempt <vincenthoang111@gmail.com>
2025-03-17 06:05:34 +0000

    main

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,3 +1,29 @@
 #include "dijkstras.h"
+#include <iostream>
+#include <exception>
+using namespace std;
 
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+    string filename = (argc > 1) ? argv[1] : "../src/small.txt";
+    Graph G;
+    try {
+        file_to_graph(filename, G);
+    } catch(const exception& e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
+
+    vector<int> previous;
+    auto dist = dijkstra_shortest_path(G, 0, previous);
+
+    for (int v = 0; v < G.numVertices; v++) {
+        if (dist[v] == INF) {
+            cout << "No path from 0 to " << v << endl;
+        } else {
+            auto path = extract_shortest_path(dist, previous, v);
+            print_path(path, dist[v]);
+        }
+    }
+
+    return 0;
+}
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -3,4 +3,24 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {}
+int main(int argc, char* argv[]) {
+    verify_word_ladder();
+
+    // if (argc < 3) {
+    //     cerr << "Usage: " << argv[0] << " <start_word> <end_word>\n";
+    //     return 1;
+    // }
+    // string start = argv[1];
+    // string end   = argv[2];
+
+    // for (auto &c : start) c = tolower((unsigned char)c);
+    // for (auto &c : end)   c = tolower((unsigned char)c);
+
+    // set<string> dictionary;
+    // load_words(dictionary, "words.txt");
+
+    // auto ladder = generate_word_ladder(start, end, dictionary);
+    // print_word_ladder(ladder);
+
+    return 0;
+}
